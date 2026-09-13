dd94cb2bf1d2d9dafda7c36cf3ceee6488a195a0
Diamond-Thomas <diamondmthomas11@gmail.com>
2026-09-13 12:29:41 -0500

    Phase 2 complete

--- Journal.md
+++ Journal.md
@@ -2,3 +2,8 @@
 
 One advantage grouping related data into an object has is that it makes it easier to understand what the data is connected to. If you where to have multiple objects that you needed the user to create a name for, having to create multiple bindings names to store the values into would be more tedious that simply creating one object and puting all the data in that object.
 
+### What happens if you set a value to a property of an object that didn’t exist before? How does that effect the ways that you can create and set object properties?
+
+if you set a value to a property of an object that didn’t exist before java script will add that property to the object along with the value. this effects the ways that you can create and set object properties because this means that you don't have to set everything up at the initialization and you can add properties as the user goes through the code and add on based on the imput from the user.
+
+
--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let storeInfo = {
     name : prompt("Enter the store name:"),
-    taxRate : prompt("Enter the stores tax rate:"),
-    tableFee : prompt("Enter the stores table fee:")
+    taxRate :  Number(prompt("Enter the stores tax rate:")),
+    tableFee :  Number(prompt("Enter the stores table fee:"))
 }
 
-Console.log("Welcome to " + storeInfo.name + ". The tax rate is " + storeInfo.taxRate + " and the table fee is " + storeInfo.tableFee + ".");
+console.log("Welcome to " + storeInfo.name + ". The tax rate is " + storeInfo.taxRate + " and the table fee is " + storeInfo.tableFee + ".");
+
+receiptItems = [];
+let addItem = true;
+while(addItem == true){
+    let choice = prompt("Press y to add an item and any other key to exit.");
+
+    if(choice == "y" || choice == "Y"){ 
+        let itemName = prompt("Enter item name:");
+        let itemPrice = Number(prompt("Enter item price:"));
+        receiptItems.push({
+            name: itemName,
+            price: itemPrice
+        });
+    } else {
+        addItem = false;
+    }
+
+
+}
+
+console.log("Number of items added: ", receiptItems.length);
+console.log("Receipt Items: ", receiptItems);
